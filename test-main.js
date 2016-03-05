c8f25feb85b7dbb32a4351734899d3862f21692c
twer <xiaotaozi1127@hotmail.com>
2016-03-05 12:59:28 +0800

    taohui remove test-main.js to baseurl

--- test-main.js
+++ test-main.js
@@ -4,7 +4,7 @@ var TEST_REGEXP = /(spec|test)\.js$/;
 
 // // Get a list of all the test files to include
 Object.keys(window.__karma__.files).forEach(function(file) {
-  if (TEST_REGEXP.test(file)) {   
+  if (TEST_REGEXP.test(file)) {
     // Normalize paths to RequireJS module names.
     // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
     // then do not normalize the paths
